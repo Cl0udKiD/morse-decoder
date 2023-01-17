1504e3a282f715086abaf05551a89a8e70382514
Cl0udKiD <sasha.azarov8@gmail.com>
2023-01-17 15:37:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MORSE_TABLE = {
+const lib = {
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -35,10 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':      ' '
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    var arr=[];
+    for (let i=0;i<str.length;++i){
+        if ((i+1)%10==0) {arr.push(str.substring(i-9,i+1))}
+    }
+    marr=[]
+    for (let i in arr){
+        morse=''
+        if (arr[i]=='**********') {morse=' '}else{
+            for (let j=0;j<arr[i].length;++j){
+                if (j%2==0) {
+                    if (arr[i].substring(j,j+2)=='10') morse+='.'
+                    if (arr[i].substring(j,j+2)=='11') morse+='-'
+                }
+            }
+        }    
+        marr.push(morse)
+    }
+    res=[]
+    for (let i in marr){
+        res.push(lib[marr[i]])
+    }
+    return res.join('')
 }
 
 module.exports = {
